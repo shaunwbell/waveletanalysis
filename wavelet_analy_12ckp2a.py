9d112713df5231eff2f13b99de27ca57e93175c8
jeepsterboy <shaun.bell@noaa.gov>
2014-07-02 09:51:52 -0700

    periodic update

---
+++ wavelet_analy_12ckp2a.py
@@ -0,0 +1,254 @@
+#!/usr/bin/env 
+"""
+ Based on waveletanalysis.py
+ 
+ For Adam
+ 
+ One station, One Year - many depth levels
+ 
+
+   Using Anaconda packaged Python
+   modifications for confidence intervals based on wave_matlab at
+   http://paos.colorado.edu/research/wavelets/
+   
+### License ###
+
+The MIT License (MIT)
+
+Copyright (c) 2013 Aaron O'Leary (dev@aaren.me)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this 
+software and associated documentation files (the "Software"), to deal in the Software 
+without restriction, including without limitation the rights to use, copy, modify, merge, 
+publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons 
+to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies 
+or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
+INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, 
+TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
+OR OTHER DEALINGS IN THE SOFTWARE.
+"""
+
+#Standard packages
+import os, datetime
+
+#Science packages
+import numpy as np
+
+#User Packages
+import wave_sig
+from general_utilities.wavelets_bams.wavelets import WaveletAnalysis
+import wavelet_analy_plot_hourly as wavelet_analy_plot
+
+__author__   = 'Shaun Bell'
+__email__    = 'shaun.bell@noaa.gov'
+__created__  = datetime.datetime(2014, 01, 13)
+__modified__ = datetime.datetime(2014, 04, 16)
+__version__  = "0.1.0"
+__status__   = "Development"
+
+"""----------------------methods for reading raw data----------------------------------"""
+
+def ADCP_2D(datafilein, timefilein, depthfilein,level, matlab_timeoffset=366.):
+    """ 
+    datafile used yyyy_BV.txt - n rows, m columns for time/depth
+    timefile used yyyy_date.txt - n rows, 1 columns for time
+    depthfile used is yyyy_depth.txt - nrows, 1 column for depth
+    TAPS dataset from Adam
+    """
+
+    data = np.loadtxt(datafilein, unpack=True)
+    time = np.loadtxt(timefilein, unpack=True) - matlab_timeoffset
+    deptharray = np.loadtxt(depthfilein, unpack=True)
+    
+    ###data massaging
+    dt = 1. #data is hourly
+    time_base = 'hours'
+    depth = -1. * deptharray
+
+    # signal
+    xx = data[:,level]
+
+    variance = np.var(xx)
+
+   
+    #normalize
+    print 'Variance = %s ' % (variance)
+    x = (xx - np.mean(xx)) / np.sqrt(variance)
+    variance = np.var(x)
+
+    sl = len(x)
+    #time = (np.arange(0,sl ,1) * dt/24. ) + startdate #date_object = datetime.datetime.strptime('04 23 2009', '%m %d %Y')
+
+    return (data, x,dt,np.array(time) * 24., variance, time_base, depth) 
+
+def ADCP_2D_subset(datafilein, timefilein, depthfilein, t_min, t_max, level, matlab_timeoffset=366.):
+    """ 
+    datafile used yyyy_BV.txt - n rows, m columns for time/depth
+    timefile used yyyy_date.txt - n rows, 1 columns for time
+    depthfile used is yyyy_depth.txt - nrows, 1 column for depth
+    TAPS dataset from Adam
+    Specify bounding times as python ordinaltimes
+    """
+    data = np.loadtxt(datafilein, unpack=True)
+    time = np.loadtxt(timefilein, unpack=True) - matlab_timeoffset
+    t_ind = (time<t_max) & (time>=t_min)
+    time = time[t_ind]
+    
+    deptharray = np.loadtxt(depthfilein, unpack=True)
+    
+    ###data massaging
+    dt = 1. #data is hourly
+    time_base = 'hours'
+    depth = -1. * deptharray
+
+    # signal
+    xx = data[t_ind,level]
+
+    variance = np.var(xx)
+
+   
+    #normalize
+    print 'Variance = %s ' % (variance)
+    x = (xx - np.mean(xx)) / np.sqrt(variance)
+    variance = np.var(x)
+
+    sl = len(x)
+    #time = (np.arange(0,sl ,1) * dt/24. ) + startdate #date_object = datetime.datetime.strptime('04 23 2009', '%m %d %Y')
+
+    return (data, x,dt,np.array(time) * 24., variance, time_base, depth)     
+    
+"""---------------------------------modules--------------------------------------------"""
+
+
+def acf(series):
+    """Determine autocorrelation factors"""
+    n = len(series)
+    data = np.asarray(series)
+    mean = np.mean(data)
+    c0 = np.sum((data - mean) ** 2) / float(n)
+
+    def r(h):
+        acf_lag = ((data[:n - h] - mean) * (data[h:] - mean)).sum() / float(n) / c0
+        return round(acf_lag, 3)
+    x = np.arange(n) # Avoiding lag 0 calculation
+    acf_coeffs = map(r, x)
+    return acf_coeffs  
+    
+"""------------------------------ Data Selection --------------------------------------"""
+for level in range(0,11,1):
+    print level
+
+    (raw_data, x, dt, time, variance, time_base, depth) = \
+    ADCP_2D('/Users/bell/Data_Local/from_phyllis/WaveletAnalysisBS2/wcp_data/12ckp2a/12ckp2a_ABS.txt', \
+    '/Users/bell/Data_Local/from_phyllis/WaveletAnalysisBS2/wcp_data/12ckp2a/12ckp2a_dates.txt', \
+    '/Users/bell/Data_Local/from_phyllis/WaveletAnalysisBS2/wcp_data/12ckp2a/12ckp2a_depth.txt', level=level) 
+    fig_name_base = 'images/12ckp2a_ADCP'
+
+    """-----------------------------wavelet analysis           ---------------------------"""
+
+    wa = WaveletAnalysis(x, time=time, dt=dt, dj=0.125)
+
+    # wavelet power spectrum
+    power = wa.wavelet_power
+    transform = wa.wavelet_transform
+
+    # scales 
+    scales = wa.scales
+
+    # associated time vector
+    t = wa.time / 24.
+
+    # reconstruction of the original data
+    rx = wa.reconstruction()
+
+    # determine acor factor for red noise
+    acorr = acf(x)
+    lagf = (acorr[1]+np.sqrt(acorr[2]))/2
+    print 'acorr lagf is %s' % lagf
+
+    # determine significance levels
+    (signif, fft_theory) = wave_sig.wave_signif(x,dt,scales,lag1=lagf)
+    sig95 = np.ones_like(power) * np.array([signif] * len(t)).transpose()
+    sig95 = power / sig95         # where ratio > 1, power is significant
+
+    # Global wavelet spectrum & significance levels:
+    global_int = variance*(np.sum(power, axis=0) ) / x.shape[0]   # time-average over all times
+    gs = ((np.sum(power, axis=1) ) / x.shape[0]) / variance #assume var=1
+    gswa = wa.global_wavelet_spectrum
+    # Global wavelet significance
+    (signif_g, fft_theory_g) = wave_sig.global_wave_signif(x,dt,scales,lag1=lagf,sigtest=1, dof=len(x))
+
+
+
+
+    """----------------------------- plot setup ------------------------------------------"""
+    T, S = np.meshgrid(t, scales)
+
+
+    """----------- plotting WaveTransform Power with confidence interval contour ----------"""
+
+    plt, fig = wavelet_analy_plot.plot_wavetransf(wa, T, S, sig95, time_base, plot_percentile=True)
+
+    plt.savefig((fig_name_base + '_wave' + str(depth[level]).replace('.0','m') + '.png'), bbox_inches='tight', dpi = (100))
+    plt.close()
+
+    """----------------- plotting contours w/global and timeseries ----------"""
+
+    plt, fig = wavelet_analy_plot.plot_wavetransf_time_zoom(x, wa, T, S, sig95, gs, signif_g,
+             time_base, scalemin=2, scalemax=256, ylabel='Echo Intens.', plot_percentile=True)
+    plt.savefig(fig_name_base + '_wave2' + str(depth[level]).replace('.0','m') + '.png', bbox_inches='tight', dpi = (100))
+    plt.close()
+
+    """----------------- plotting contours w/global and timeseries ----------"""
+    """----------------- zoom in on time series ----------"""
+    
+    t_min = datetime.datetime.strptime('2013-05-15','%Y-%m-%d').toordinal() 
+    t_max = datetime.datetime.strptime('2013-09-15','%Y-%m-%d').toordinal() 
+    months = 'JJA'
+    plt, fig = wavelet_analy_plot.plot_wavetransf_time_zoom_tbound(x, wa, T, S, sig95, gs, signif_g,
+             time_base, t_min, t_max, scalemin=2, scalemax=256, ylabel='Echo Intens.', plot_percentile=True)
+    plt.savefig(fig_name_base + '_wave_' + months + str(depth[level]).replace('.0','m') + '.png', bbox_inches='tight', dpi = (100))
+    plt.close()
+    
+    """----------------------- plotting power spectrum FFT --------------------------------"""
+    (plt, fig) = wavelet_analy_plot.fft_power_spec(x, time_base, Fs=1)
+
+    plt.savefig(fig_name_base + '_FFTspec' + str(depth[level]).replace('.0','m') + '.png', bbox_inches='tight', dpi = (100))
+    plt.close()
+
+    """
+    # Do FFT analysis of array
+    sp = np.fft.fft(x)
+    # Getting the related frequencies
+    freq = np.fft.fftfreq(t.shape[-1], d=.25)
+    pyy = sp*np.conj(sp)
+    """
+
+    """----------------- plot scale averaged power timeseries -----------------------------"""
+    #Need to know what scales to plot
+    scales_bin = [8, 30]
+
+    #find indices
+    indices = np.where((scales >8) & (scales < 30))
+
+    #scales[indices]
+    scale_ave1 = power[indices].mean(axis=0)
+
+    (plt, fig) = wavelet_analy_plot.scale_ave_timeseries(scale_ave1, wa.time / 24. , scales_bin)
+    plt.savefig(fig_name_base + '_scaleave' + str(depth[level]).replace('.0','m') + '.png', bbox_inches='tight', dpi = (100))
+    plt.close()
+
+    """-------------------------- plot orig data (2d) -------------------------------------"""
+
+(plt, fig) = wavelet_analy_plot.plot2dvar(raw_data, time / 24., depth)
+plt.savefig((fig_name_base + '_rawdata' + str(depth[level]).replace('.0','m') + '.png'), bbox_inches='tight', dpi = (100))
+plt.close()
+
+
+
--- wavelet_analy_plot_hourly.py
+++ wavelet_analy_plot_hourly.py
@@ -16,10 +16,13 @@
 import os
 
 #Science packages
-import matplotlib.pyplot as plt
-from matplotlib.dates import DateFormatter
 import numpy as np
 from scipy import stats
+
+#Visual Packages
+import matplotlib.pyplot as plt
+from matplotlib.dates import DateFormatter
+from matplotlib.dates import MonthLocator
 
 #Other Packages
 import brewer2mpl as b2m #colorbrewer maps
@@ -28,7 +31,7 @@ import brewer2mpl as b2m #colorbrewer maps
 
 #set up color map with brewer2mpl, not mandatory as matplotlib as the colorbrewer maps but
 # provides an easy way to limit the number of colors in a map
-bmap = b2m.get_map('Greens', 'Sequential', 5, reverse=False)
+bmap = b2m.get_map('Blues', 'Sequential', 5, reverse=False)
 bmap = bmap.mpl_colors
 
 """----------------------------- plots  ----------------------------------------------"""
@@ -70,6 +73,8 @@ def plot_wavetransf(wa, T, S, sig95, time_base, plot_percentile=False):
     fourier_lim = [wa.fourier_period(i) for i in ax.get_ylim()]
     ax_fourier.set_ylim(fourier_lim)
 
+    ax.xaxis.set_minor_locator(MonthLocator(bymonthday=(1,15)))
+    ax.xaxis.set_major_locator(MonthLocator(bymonthday=1, interval=2))
     ax.xaxis.set_major_formatter(DateFormatter('%Y-%m-%d'))
     #fig.autofmt_xdate()
 
@@ -112,6 +117,8 @@ def plot_wavetransf_time(x, wa, T, S, sig95, gs, signif_g, time_base, ylabel='Pr
     ax.yaxis.set_ticklabels(ticks.astype(str))
     ax.set_ylim(256, 0.5)
 
+    ax.xaxis.set_minor_locator(MonthLocator(bymonthday=(1,15)))
+    ax.xaxis.set_major_locator(MonthLocator(bymonthday=1, interval=2))
     ax.xaxis.set_major_formatter(DateFormatter('%Y-%m-%d'))
     #fig.autofmt_xdate()
 
@@ -189,6 +196,8 @@ def plot_wavetransf_time_zoom(x, wa, T, S, sig95, gs, signif_g, time_base, scale
     ax.yaxis.set_ticklabels(ticks.astype(str))
     ax.set_ylim(scalemax, scalemin)
 
+    ax.xaxis.set_minor_locator(MonthLocator(bymonthday=(1,15)))
+    ax.xaxis.set_major_locator(MonthLocator(bymonthday=1, interval=2))
     ax.xaxis.set_major_formatter(DateFormatter('%Y-%m-%d'))
     #fig.autofmt_xdate()
 
@@ -239,6 +248,74 @@ def plot_wavetransf_time_zoom(x, wa, T, S, sig95, gs, signif_g, time_base, scale
     
     return(plt, fig)    
 
+def plot_wavetransf_time_zoom_tbound(x, wa, T, S, sig95, gs, signif_g, time_base, t_min, t_max, scalemin=0, \
+    scalemax=6, ylabel='Pressure (mb)', plot_percentile=False):
+    """plotting contours w/global and timeseries with specified time limits"""
+
+    fig = plt.figure(22)
+    ax = plt.subplot2grid((3, 4), (1, 0), colspan=3, rowspan=2)
+    # use following with unnormalized data to match web output
+    if plot_percentile:
+        #use following to contour at "percentiles variances" when using non-normalized data to match web output
+        csf =plt.contourf(T, S, wa.wavelet_power, levels=[ 0, stats.scoreatpercentile(wa.wavelet_power, 25), stats.scoreatpercentile(wa.wavelet_power, 50),
+                                           stats.scoreatpercentile(wa.wavelet_power, 75), stats.scoreatpercentile(wa.wavelet_power, 95), 
+                                           stats.scoreatpercentile(wa.wavelet_power, 100)], colors=bmap)
+    else:
+        levels = [0,1,5,100,250,500,750]
+        csf =plt.contourf(T, S, wa.wavelet_power, levels=levels, colors=bmap)
+    levels = [-99, 1] # values greater than 1 are significant
+    plt.contour(T, S, sig95,levels, colors='black', linewidth=5)
+    ax.set_yscale('log')
+    ax.set_ylabel('Scales')
+    ax.grid(True)
+
+    # put the ticks at powers of 2 in the scale
+    ticks = np.unique(2 ** np.floor(np.log2(wa.scales)))[1:]
+    ax.yaxis.set_ticks(ticks)
+    ax.yaxis.set_ticklabels(ticks.astype(str))
+    ax.set_ylim(scalemax, scalemin)
+
+    ax.xaxis.set_minor_locator(MonthLocator(bymonthday=(1,15)))
+    ax.xaxis.set_major_locator(MonthLocator(bymonthday=1, interval=1))
+    ax.xaxis.set_major_formatter(DateFormatter('%Y-%m-%d'))
+    #fig.autofmt_xdate()
+
+    # shade the region between the edge and coi
+    C, S = wa.coi
+    C = C /24.
+    ax.fill_between(x=C, y1=S, y2=wa.scales.max(), color='gray', alpha=0.5)
+    ax.set_xlim(t_min, t_max)
+
+    ax = plt.subplot2grid((3, 4), (1, 3), colspan=1, rowspan=2)
+    p1 = ax.plot(gs,wa.scales, signif_g, wa.scales, 'k--')
+    ax.set_yscale('log')
+    ax.set_xscale('log')
+    ax.grid(True)
+
+    # put the ticks at powers of 2 in the scale
+    ticks = np.unique(2 ** np.floor(np.log2(wa.scales)))[1:]
+    ax.yaxis.set_ticks(ticks)
+    ax.set_yticklabels([])
+    #ax.yaxis.set_ticklabels(ticks.astype(str))
+    ax.set_ylim(scalemax, scalemin)
+
+    # second y scale with equivalent fourier periods to scales
+    # except with the ticks at the powers of 2
+    ax_fourier = ax.twinx()
+    ax_fourier.set_yscale('log')
+    # match the fourier ticks to the scale ticks
+    ax_fourier.set_yticks(ticks)
+    ax_fourier.set_yticklabels(ticks.astype(str))
+    ax_fourier.set_ylabel('fourier period (%s)' % time_base )
+    fourier_lim = [wa.fourier_period(i) for i in ax.get_ylim()]
+    ax_fourier.set_ylim(fourier_lim)
+
+    #plt.show()
+    DefaultSize = fig.get_size_inches()
+    fig.set_size_inches( (DefaultSize[0]*2, DefaultSize[1]) )
+    
+    return(plt, fig)    
+    
 def scaleogram(wa):
     """scaleogram"""
 
@@ -249,6 +326,8 @@ def scaleogram(wa):
     ax.set_ylim(ax.get_ylim()[::-1]) #this reverses the yaxis (i.e. deep at the bottom)
     cbar = plt.colorbar()
     
+    ax.xaxis.set_minor_locator(MonthLocator(bymonthday=(1,15)))
+    ax.xaxis.set_major_locator(MonthLocator(bymonthday=1, interval=2))
     ax.xaxis.set_major_formatter(DateFormatter('%Y-%m-%d'))
     fig.autofmt_xdate()
 
@@ -278,6 +357,8 @@ def scale_ave_timeseries(scale_ave, time, scales_bin):
     ax1.grid(True)
     ax1.set_ylabel('Average power between scales ' + str(scales_bin[0]) + ' and ' + str(scales_bin[1]) + '')
     ax1.set_xlabel('Time (UTC)')
+    ax1.xaxis.set_minor_locator(MonthLocator(bymonthday=(1,15)))
+    ax1.xaxis.set_major_locator(MonthLocator(bymonthday=1, interval=2))
     ax1.xaxis.set_major_formatter(DateFormatter('%Y-%m-%d'))
 
     #plt.show()
@@ -296,6 +377,8 @@ def scale_ave_timeseries2D(scale_ave1, scale_ave2, time1, time2, scales_bin):
     ax1.grid(True)
     ax1.set_ylabel('Average power between scales ' + str(scales_bin[0]) + ' and ' + str(scales_bin[1]) + '')
     ax1.set_xlabel('Time (UTC)')
+    ax1.xaxis.set_minor_locator(MonthLocator(bymonthday=(1,15)))
+    ax1.xaxis.set_major_locator(MonthLocator(bymonthday=1, interval=2))
     ax1.xaxis.set_major_formatter(DateFormatter('%Y-%m-%d'))
 
     #plt.show()
@@ -314,6 +397,8 @@ def plot2dvar(data, x, y):
     ax.set_ylim(ax.get_ylim()[::-1]) #this reverses the yaxis (i.e. deep at the bottom)
     cbar = plt.colorbar()
 
+    ax.xaxis.set_minor_locator(MonthLocator(bymonthday=(1,15)))
+    ax.xaxis.set_major_locator(MonthLocator(bymonthday=1, interval=2))
     ax.xaxis.set_major_formatter(DateFormatter('%Y-%m-%d'))
     fig.autofmt_xdate()
     DefaultSize = fig.get_size_inches()
